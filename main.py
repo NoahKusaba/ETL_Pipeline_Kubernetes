44e365751ae9322304bd505e5a83ac7b0aa0c62c
Noah Kusaba <noahskusaba@gmail.com>
2023-10-01 21:00:48 -0400

    first_commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from prefect import flow
+import requests
+from boto3 import client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+id = os.getenv('ID')
+key = os.getenv('KEY')
+@flow(name ="Get_Activity")
+def main():
+    activity_suggestion = requests.get("https://www.boredapi.com/api/activity/").json()['activity']
+    send_email(activity_suggestion)
+
+def send_email(activity):
+    email_client = client('ses', region_name='us-east-1', aws_access_key_id= id, aws_secret_access_key= key)
+    response = email_client.send_email(
+    Destination={ 'ToAddresses': ['nkusaba@bearcognition.com'] },
+    Message={
+        'Body': {
+            'Text': {
+                'Charset': 'UTF-8',
+                'Data': activity,
+            }
+        },
+        'Subject': {
+            'Charset': 'UTF-8',
+            'Data': f'Noah-Kusaba - Activity Suggestion',
+        },
+    },
+    Source='nkusaba@bearcognition.com'
+    )
+
+main()
