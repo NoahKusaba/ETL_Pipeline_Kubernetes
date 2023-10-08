6427158332128aaec396b1d0e8e7ef1f6e543456
Noah Kusaba <noahskusaba@gmail.com>
2023-10-08 15:15:50 -0400

    add prefect block

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from prefect import flow, get_run_logger
+from prefect import flow
+from prefect.blocks.system import Secret
+from json import loads
 import requests
 from boto3 import client
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+secret_block = Secret.load("aws-creds") ## Change This for HIPAA
+secret_response = secret_block.get()
+aws_auth =  loads(secret_response)
+id = aws_auth['id']
+key = aws_auth['key']
 
 @flow(name ="Get_Activity")
 def main():
@@ -13,11 +19,7 @@ def main():
     send_email(activity_suggestion)
 
 def send_email(activity):
-    logger = get_run_logger()
-    id = os.getenv('ID')
-    key = os.getenv('KEY')
-    print(id)
-    logger.info(id)
+
     email_client = client('ses', region_name='us-east-1', aws_access_key_id= id, aws_secret_access_key= key)
     response = email_client.send_email(
     Destination={ 'ToAddresses': ['nkusaba@bearcognition.com'] },
